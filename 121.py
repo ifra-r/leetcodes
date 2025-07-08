2660a79a16f01eb1112d6409d8f5895b8c38dc67
Ifra <ifraaabdulrauf@gmail.com>
2025-07-09 00:36:17 +0500

    day 15 ig

---
+++ 121.py
@@ -0,0 +1,15 @@
+# 121. best time to buy and sell
+
+class Solution(object):
+    def maxProfit(self, prices):
+        min_price = float('inf')
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price   
+            else:
+                profit = price - min_price   
+                max_profit = max(max_profit, profit)
+
+        return max_profit
