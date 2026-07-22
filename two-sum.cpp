28d5df48e03c93b6199e5cbff7dd355628956d85
Ifra <ifraaabdulrauf@gmail.com>
2026-07-22 23:47:54 +0500

    two sum naive approach

---
+++ two-sum.cpp
@@ -0,0 +1,18 @@
+
+
+
+// Naive approach
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       for (int i=0; i<nums.size(); i++) {
+        for (int j=0; j<nums.size(); j++) {
+            if (i==j) continue;
+            int sum = nums[i] + nums[j];
+            if (sum==target)
+                return {i,j};
+        }
+       } 
+       return {};
+    }
+};
