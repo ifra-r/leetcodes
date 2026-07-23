efdc8bf98af2a0de0457767e343a7e9d08b6e647
Ifra <ifraaabdulrauf@gmail.com>
2026-07-23 23:53:20 +0500

    two sum improved approach; hash map, 2 ptr approach

--- two-sum.cpp
+++ two-sum.cpp
@@ -1,4 +1,45 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        for (int i=0; i<nums.size(); ++i) {
+            int val = nums[i];
 
+            if (map.find(target-val) != map.end())
+                return {i, map[target-val]};
+            map[val] = i;   // add value
+        }
+        return {};
+    }
+};
+
+
+// 2 ptr approach with (value, index pair)
+
+class Solution1 {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // make (value, index) pair and sort
+        // sort --> to use 2 ptr approach
+        // (value, index) --> to keep track of index as it will be lost after sorting
+        vector<pair<int,int>> v;
+        for (int i = 0; i < nums.size(); i++)
+            v.push_back({nums[i], i});   // {value, index}
+
+        sort(v.begin(), v.end());
+
+        int left = 0, right = v.size()-1;
+        while (left < right) {
+            int sum = v[left].first + v[right].first;
+            if (sum == target) return {v[left].second, v[right].second};
+            if (sum > target)
+                right--;    // make sum smaller, move to smaller elements
+            else
+                left++;     // make sum larger next time
+        }
+        return {};
+    }
+};
 
 
 // Naive approach
